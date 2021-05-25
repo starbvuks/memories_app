0d1125236b2c3cc0071714719cb688a615d292f0
starbvuks <starbvuks@gmail.com>
2021-05-25 17:19:07 +0530

    component heiarchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
 
+import memories from "./components/assets/dreampop.jpg";
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
 const App = () => {
   return (
-    <div>
-      <h1>Hey</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="strech"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <h1>Forms</h1>
+    </>
+  );
+};
+
+export default Form;
